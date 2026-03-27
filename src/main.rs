77a5e3429113de372b922fc0d68167925fa3948b
Martin John M <johnmmartin74@gmail.com>
2026-03-27 11:20:46 +0100

    A basic app and listener to receive the initial http connection which eventually converts to full duplex websocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::sync::Arc;
+
+use axum::{Router, routing::get};
 use tokio::sync::broadcast;
 
 
@@ -12,5 +15,24 @@ struct AppState {
 
 #[tokio::main]
 async fn main() {
+
+    // 1. if a client is too slow and lags 16 messages it will drop
+    let (tx, rx) = broadcast::channel(16);
+    let app_state = Arc::new(AppState { tx});
+
+    // 2. the router
+    let app = Router::new()
+        .route("/chat", get(handler))
+        .with_state(app_state);
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+
+    println!("Server started ae ws://127.0.0.1:3000/chat");
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() {
 
 }
